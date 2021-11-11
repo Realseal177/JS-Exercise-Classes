32e437bc0e7be0f148f2167f3596226ceaef7e17
Andrew Nelson <Realseal177@gmail.com>
2021-11-11 12:51:47 -0800

    most of 2 done. 3 complete

--- index.js
+++ index.js
@@ -76,7 +76,22 @@ class Person {
 */
 
 class Car {
-  
+  constructor(model, milesPerGallon){
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = this.tank + gallons;
+  }
+  drive(distance){
+    this.odometer = this.odometer + distance;
+    this.tank = this.tank - (distance / this.milesPerGallon);
+    if (distance > this.tank * this.milesPerGallon){
+      return `I ran out of fuel at ${this.odometer} miles`;
+    }
+  }
 }
 
 /*
@@ -92,7 +107,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-  
+  constructor(obj){
+    this.name = obj.name;
+    this.age = obj.age;
+    this.location = obj.location;
+  }
+  speak(){
+    return `Hello, my name is ${this.name}, I am from ${this.location}`
+  }
 }
 
 /*
